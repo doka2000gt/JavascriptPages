597d9783a0a1b8baea7c837950d982815e667189
doka2000gt <doka2000gt@yahoo.co.jp>
2023-04-13 17:37:05 +0900

    reimuAni

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+let reimu;
+
+function preload() {
+	// 1, アニメーション
+	loadAni("r_good", "img/reimu_good_01.png", 3);
+}
+
 function setup(){
 	createCanvas(windowWidth, windowHeight);
-	frameRate(8);
+	frameRate(32);
+	noSmooth();
+
+	// スプライト(霊夢)
+	reimu = new Sprite(width/2, height/2, 45);
+	reimu.shapeColor = color("white");
+	reimu.ani = "r_good";// 2, アニメーションの適用
+	reimu.scale = 4;// 3, スケールの変更
 }
 
 function draw(){
-	background(33);
-	fill(255);
-	circle(width/2, height/2, 100);
+	background(0);
 }
